42cca8b31c701fc9ef8d450353745c5c961d6207
unknown <rachelrvasquez@gmail.com>
2026-02-09 15:50:28 -0500

    working on breaking down the header, a bit more tricky...

--- header.php
+++ header.php
@@ -15,14 +15,14 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php 
+<?php
 	do_action( 'memberlite_before_page' );
-	
+
 	if ( function_exists( 'wp_body_open' ) ) {
 		wp_body_open();
 	} else {
 		do_action( 'wp_body_open' );
-	} 
+	}
 ?>
 <div id="page" class="hfeed site">
 
@@ -30,7 +30,9 @@
 	// Hide header output for the Blank page template.
 	if ( ! is_page_template( 'templates/blank.php' ) ) { ?>
 
-	<?php get_template_part( 'components/header/mobile', 'menu' ); ?>
+	<?php
+    //@todo: Can we combine this with the site navigation part or just re-style so it's both desktop/mobile friendly?
+    //get_template_part( 'components/header/mobile', 'menu' ); ?>
 
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'memberlite' ); ?></a>
 
@@ -38,22 +40,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="row">
-			<?php
-				$meta_login = get_theme_mod( 'meta_login', false );
-				if ( ! is_page_template( 'templates/interstitial.php' ) && ( ! empty( $meta_login ) || has_nav_menu( 'meta' ) || is_active_sidebar( 'sidebar-3' ) ) ) {
-					$show_header_right = true;
-				} else {
-					$show_header_right = false;
-				}
-				/**
-				 * Filter to hide or show the right column area of the header.
-				 *
-				 * @param bool $show_header_right True to show the header right, false to hide it.
-				 *
-				 * @return bool $show_header_right
-				 */
-				$show_header_right = apply_filters( 'memberlite_show_header_right', $show_header_right );
-			?>
+            <?php get_template_part( 'components/header/header', 'meta-login' ); ?>
 
 			<div class="
 			<?php
@@ -122,52 +109,11 @@
 				<?php }
 			?>
 		</div><!-- .row -->
+
+        <?php get_template_part( 'components/header/header', 'site-navigation' ); ?>
+
+       <?php } // End if(). ?>
 	</header><!-- #masthead -->
-
-	<?php do_action( 'memberlite_before_site_navigation' ); ?>
-
-	<?php if ( ! is_page_template( 'templates/interstitial.php' ) && has_nav_menu( 'primary' ) ) { ?>
-		<?php
-			$sticky_nav = get_theme_mod( 'sticky_nav' );
-			if ( $sticky_nav == true ) { ?>
-				<div class="site-navigation-sticky-wrapper">
-			<?php }
-		?>
-		<nav id="site-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Main Menu', 'memberlite' ); ?>">
-		<?php
-		if ( has_nav_menu( 'primary' ) ) {
-			$primary_defaults = array(
-				'theme_location'  => 'primary',
-				'container'       => false,
-				'fallback_cb'     => false,
-				'items_wrap'      => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>',
-				'menu_class'      => 'menu',
-				'walker'          => new Memberlite_Aria_Walker_Nav_Menu(),
-			);
-			wp_nav_menu( $primary_defaults );
-		}
-		?>
-		</nav><!-- #site-navigation -->
-		<?php
-			if ( $sticky_nav == true ) { ?>
-			</div> <!-- .site-navigation-sticky-wrapper -->
-			<script>
-				jQuery(document).ready(function ($) {
-					var s = $("#site-navigation");
-					var pos = s.position();
-					$(window).scroll(function() {
-						var windowpos = $(window).scrollTop();
-						if ( windowpos >= pos.top ) {
-							s.addClass("site-navigation-sticky");
-						} else {
-							s.removeClass("site-navigation-sticky");
-						}
-					});
-				});
-			</script>
-		<?php }
-		}
-	} // End if(). ?>
 
 	<?php do_action( 'memberlite_before_content' ); ?>
 
